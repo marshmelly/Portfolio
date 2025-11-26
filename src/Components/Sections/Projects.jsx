05f7915798babc4d19fe4af26d5129b86aef4eb3
marshmelly <rolfensonfobbs05@gmail.com>
2025-11-26 17:06:00 +0300

    Second Revision

--- src/Components/Sections/Projects.jsx
+++ src/Components/Sections/Projects.jsx
@@ -27,7 +27,7 @@ export const Projects = () => {
                     
 
                     <div className="">
-                        {["React", "Node.js", "Firebase"].map((tech, key) => (
+                        {["React", "Node.js", "Firebase", "Email.js"].map((tech, key) => (
                         <span
                              key={key}
                             className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
@@ -47,11 +47,10 @@ export const Projects = () => {
                     <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/70
                                     hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
                         <h3 className="text-xl font-bold mb-2">
-                            <strong>El-Arts</strong>
+                            <strong>Glow Up Salon</strong>
                         </h3>
                         <p className="text-gray-300 mb-4">
-                            A dynamic website that sells rigs and images used for animation,
-                            graphic design for users who want proffessional rigs for their use.
+                            A dynamic website lets you book and set up a date for your hair day.
                         </p>
                     
 
@@ -67,7 +66,7 @@ export const Projects = () => {
                     </div>
 
                     <div className="flex justify-end items-center mt-4">
-                        <a href="#" className="text-blue-400 hover:text-cyan-300 transition-colors">
+                        <a href="https://glowupsalon.vercel.app/" className="text-blue-400 hover:text-cyan-300 transition-colors">
                             View Project &rarr;
                         </a>
                     </div>
@@ -97,6 +96,35 @@ export const Projects = () => {
 
                     <div className="flex justify-end items-center mt-4">
                         <a href="https://huncho-delta.vercel.app/" className="text-blue-400 hover:text-cyan-300 transition-colors">
+                            View Project &rarr;
+                        </a>
+                    </div>
+                    </div>
+
+                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/70 
+                                    hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
+                        <h3 className="text-xl font-bold mb-2">
+                            <strong>Mell's Travels</strong>
+                        </h3>
+                        <p className="text-gray-300 mb-4">
+                        Mell’s Travels helps you discover beautiful destinations, book memorable adventures, and enjoy seamless, affordable travel experiences tailored to your journey.
+                          product inventory.
+                        </p>
+                    
+
+                    <div className="">
+                        {["React", "Node.js", "Email.js", "Firebase"].map((tech, key) => (
+                        <span
+                             key={key}
+                            className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
+                                       hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
+                                         {tech}
+                                    </span>
+                    ))}
+                    </div>
+
+                    <div className="flex justify-end items-center mt-4">
+                        <a href="https://melltravels.vercel.app/" className="text-blue-400 hover:text-cyan-300 transition-colors">
                             View Project &rarr;
                         </a>
                     </div>
